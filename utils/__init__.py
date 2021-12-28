f67de117621267db2d28743eee5eebc2ff2f0c4a
lucien1011 <lucien1011@gmail.com>
2021-12-28 11:52:56 -0500

    [utils] Added seed

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,4 @@
-from .Timer import Timer
 from .mkdir import mkdir_p,mkdir_from_filepath
 from .conf import read_attr_conf
+from .seed import set_seed
+from .Timer import Timer
---
+++ utils/seed.py
@@ -0,0 +1,15 @@
+import numpy as np
+import os
+import random
+import torch
+
+def set_seed(seed):
+    """ Set all seeds to make results reproducible """
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed) 
+
